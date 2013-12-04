ef3cfe728c0430ba694bdad1ac6ee5efd9c831d8
Jakub Roztocil <jakub@roztocil.name>
2013-12-04 19:08:14 +0100

    Custom fixes.

--- apns.py
+++ apns.py
@@ -26,11 +26,15 @@
 from binascii import a2b_hex, b2a_hex
 from datetime import datetime, timedelta
 from time import mktime
-from random import getrandbits
-from socket import socket, AF_INET, SOCK_STREAM, timeout, error
+from socket import socket, AF_INET, SOCK_STREAM, timeout
 from struct import pack, unpack
 
 import os
+import string
+
+import logging
+
+logger = logging.getLogger('apns')
 
 try:
     from ssl import wrap_socket
@@ -44,8 +48,6 @@ except ImportError:
     import simplejson as json
 
 MAX_PAYLOAD_LENGTH = 256
-
-TIMEOUT = float(os.environ.get('APNS_TIMEOUT', 0.5))
 
 class APNs(object):
     """A class representing an Apple Push Notification service connection"""
@@ -116,35 +118,36 @@ class APNsConnection(object):
     """
     A generic connection class for communicating with the APNs
     """
-    def __init__(self, cert_file=None, key_file=None, timeout=TIMEOUT):
+    def __init__(self, cert_file=None, key_file=None):
         super(APNsConnection, self).__init__()
         self.cert_file = cert_file
         self.key_file = key_file
         self._socket = None
         self._ssl = None
-        self.timeout = timeout
 
     def __del__(self):
-        self._disconnect();
+        self._disconnect()
 
     def _connect(self):
         # Establish an SSL connection
+        logger.info("Connecting")
         self._socket = socket(AF_INET, SOCK_STREAM)
         self._socket.connect((self.server, self.port))
-        self._socket.settimeout(self.timeout)
         self._ssl = wrap_socket(self._socket, self.key_file, self.cert_file)
 
     def _disconnect(self):
+        logger.info("Disconnecting")
         if self._socket:
             self._socket.close()
-            self._ssl = None # Make sure we reconnect on next try
+            self._ssl = None  # Make sure we reconnect on next try
 
     def _connection(self):
         if not self._ssl:
             self._connect()
         return self._ssl
 
-    def read(self, n=None):
+    def read(self, n=None, timeout=0):
+        self._connection().settimeout(timeout)
         return self._connection().read(n)
 
     def write(self, string):
@@ -235,7 +238,7 @@ class FeedbackConnection(APNsConnection):
     def _chunks(self):
         BUF_SIZE = 4096
         while 1:
-            data = self.read(BUF_SIZE)
+            data = self.read(BUF_SIZE, timeout=0.5)
             yield data
             if not data:
                 break
@@ -275,54 +278,30 @@ class FeedbackConnection(APNsConnection):
                     # some more data and append to buffer
                     break
 
-class APNResponseError(Exception):
-    pass
+class Notification(object):
+    def __init__(self, token_hex, payload, identifier=0, expiry=None):
+        assert all(c in string.hexdigits for c in token_hex)
+        assert len(token_hex) == 64
 
-class UnknownResponse(APNResponseError):
-    def __init__(self):
-        super(UnknownResponse, self).__init__()
+        self.token_hex = token_hex
+        self.payload = payload
+        self.identifier = identifier
 
-class UnknownError(APNResponseError):
-    def __init__(self):
-        super(UnknownError, self).__init__()
+        if expiry is None:
+            self.expiry = datetime.now() + timedelta(30)
+        else:
+            self.expiry = expiry
 
-class ProcessingError(APNResponseError):
-    def __init__(self):
-        super(ProcessingError, self).__init__()
-
-class InvalidTokenSizeError(APNResponseError):
-    def __init__(self):
-        super(InvalidTokenSizeError, self).__init__()
-
-class InvalidPayloadSizeError(APNResponseError):
-    def __init__(self):
-        super(InvalidPayloadSizeError, self).__init__()
-
-class InvalidTokenError(APNResponseError):
-    def __init__(self):
-        super(InvalidTokenError, self).__init__()
-
-class GatewayConnection(APNsConnection):
-    """
-    A class that represents a connection to the APNs gateway server
-    """
-    def __init__(self, use_sandbox=False, **kwargs):
-        super(GatewayConnection, self).__init__(**kwargs)
-        self.server = (
-            'gateway.push.apple.com',
-            'gateway.sandbox.push.apple.com')[use_sandbox]
-        self.port = 2195
-
-    def _get_notification(self, token_hex, payload, identifier, expiry):
+    def get_binary(self):
         """
         Takes a token as a hex string and a payload as a Python dict and sends
         the notification
         """
-        identifier_bin = identifier[:4]
-        expiry_bin = APNs.packed_uint_big_endian(int(mktime(expiry.timetuple())))
-        token_bin = a2b_hex(token_hex)
+        identifier_bin = pack('>I', self.identifier)
+        expiry_bin = APNs.packed_uint_big_endian(int(mktime(self.expiry.timetuple())))
+        token_bin = a2b_hex(self.token_hex)
         token_length_bin = APNs.packed_ushort_big_endian(len(token_bin))
-        payload_json = payload.json()
+        payload_json = self.payload.json()
         payload_length_bin = APNs.packed_ushort_big_endian(len(payload_json))
 
         notification = ('\x01' + identifier_bin + expiry_bin + token_length_bin + token_bin
@@ -330,39 +309,131 @@ class GatewayConnection(APNsConnection):
 
         return notification
 
+    def __repr__(self):
+        return '<%s to %s id:%d>' % (repr(self.payload), self.token_hex, self.identifier)
+
+class GatewayConnection(APNsConnection):
+    """
+    A class that represents a connection to the APNs gateway server
+
+    This class will guarentee that well-formed notifications will arrive at their destination.
+    Read http://redth.info/the-problem-with-apples-push-notification-ser/ for a detailed description of the problem and the solution, implemented below
+    """
+    def __init__(self, use_sandbox=False, **kwargs):
+        super(GatewayConnection, self).__init__(**kwargs)
+        self.server = (
+            'gateway.push.apple.com',
+            'gateway.sandbox.push.apple.com')[use_sandbox]
+        self.port = 2195
+        self.next_identifier = 0
+        self.failed_notifications = []
+        self.in_flight_notifications = []
+
+    def flush(self):
+        """
+        Check for all notifications if they were correctly sent.
+        If this method is not called after your batch of notifications there might be notifications that did not arrive at their destination
+        If for example a notification somewhere in the middle of your batch is corrupted (see the response dictionary below for a list of possible problems)
+        it is possible that that notification AND ALL NOTIFICATIONS AFTER IT will be discarded by Apple.
+        """
+        if len(self.in_flight_notifications):
+            while self._check_for_errors(timeout=1):
+                pass
+
+        # If the APN server has no error codes left to tell us we can assume that all in-flight notifications have arrived
+        self.in_flight_notifications = []
+
+    def _check_for_errors(self, timeout=0):
+        """
+        Try to read error codes from the APNS gateway server.
+
+        This tells us which notification failed,
+        which succeeded (all notifications sent before the failed notification)
+        and which need to be resent (all notifications sent after the failed notification)
+
+        Will return a boolean that indicates if it sent out new notifications.
+        """
+        resended_notifications = False
+        try:
+            error_response = self.read(6, timeout=timeout)
+            if not error_response:
+                logger.info("Socket EOF")
+                status = 999
+                identifier = None
+            else:
+                logger.info("Got error_response")
+                # command = error_response[0]
+                status = ord(error_response[1])
+                identifier, = unpack('>I', error_response[2:6])
+
+            if status > 0:
+                self._disconnect()  # Make sure we're ready for next send.
+
+            in_flight = self.in_flight_notifications
+
+            # Every notification in in_flight will either succeeded, fail or be marked for resending
+            # We can start afresh with the list of in flight notifications
+            self.in_flight_notifications = []
+
+            needs_resending = []
+
+            # Check for all previously sent notifications if they arrived or not
+            for notification in in_flight:
+                if identifier and notification.identifier < identifier:
+                    # This notification was *before* the problematic notification.
+                    # We can assume it was sent successfully and don't have to do anything with it anymore
+                    pass
+                elif identifier and notification.identifier == identifier:
+                    if status > 0:
+                        # This was the notification that tripped up the APN server.
+                        # Save it to a list of failed notifications so that the API user can process them somehow
+                        response = {
+                            1: 'Processing error',
+                            2: 'Missing device token',
+                            3: 'Missing topic',
+                            4: 'Missing payload',
+                            5: 'Invalid token size',
+                            6: 'Invalid topic size',
+                            7: 'Invalid payload size',
+                            8: 'Invalid token'}.get(status, 'Unknown Error')
+
+                        notification.fail_reason = (status, response)
+
+                        self.failed_notifications.append(notification)
+                else:
+                    if status > 0:
+                        # This notification was sent *after* the problematic notification
+                        # We can assume that it was not sent and needs to be resent.
+                        needs_resending.append(notification)
+                    else:
+                        # Unless offcourse it was not problematic at all and Apple sends an "Error" that no errors were encountered (status code 0)
+                        # In that case we still don't know the fate of the notification and have to put it back in-flight
+                        self.in_flight_notifications.append(notification)
+
+            for notification in needs_resending:
+                self.send(notification)
+                resended_notifications = True
+
+        except (SSLError, timeout), ex:
+            # SSL throws SSLError instead of timeout, see http://bugs.python.org/issue10272
+            # Timeouts are OK - don't reconnect
+            logger.info("Threw exception: %s", ex)
+
+        return resended_notifications
+
     def send_notification(self, token_hex, payload, expiry=None):
-        if expiry is None:
-            expiry = datetime.now() + timedelta(30)
-        
-        identifier = pack('>I', getrandbits(32))
+        self.send(Notification(token_hex, payload, expiry))
+
+    def send(self, notification):
+        notification.identifier = self.next_identifier
+        self.next_identifier += 1
 
         try: # Connection might have been closed
-            self.write(self._get_notification(token_hex, payload, identifier, expiry))
-        except (SSLError, error):
-            # We were disconnected or timeout.
+            self.write(notification.get_binary())
+            self.in_flight_notifications.append(notification)
+        except:
             # Prepare to reconnect.
             self._disconnect()
+            raise
         else:
-            try:
-                error_response = self.read(6)
-                if error_response != '':
-                    command = error_response[0]
-                    status = ord(error_response[1])
-                    response_identifier = error_response[2:6]
-                    
-                    if command != '\x08' or response_identifier != identifier:
-                        raise UnknownResponse()
-                    
-                    if status == 0:
-                        return
-                    
-                    raise {1: ProcessingError,
-                           5: InvalidTokenSizeError,
-                           7: InvalidPayloadSizeError,
-                           8: InvalidTokenError}.get(status, UnknownError)()
-            except (SSLError, timeout), ex:
-                 # SSL throws SSLError instead of timeout, see http://bugs.python.org/issue10272
-                pass # Timeouts are OK - don't reconnect
-            except APNResponseError:
-                self._disconnect() # Make sure we're ready for next send.
-                raise
+            self._check_for_errors()
